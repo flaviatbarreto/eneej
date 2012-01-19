37fd300e050550f62338bc7123667fd12963be80
Marcelo Novaes <novaes.ssa@gmail.com>
2012-01-19 16:16:46 -0200

    Rails 3.0.10

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eneej::Application.config.secret_token = '60a4ced753e02b88a1afc4895a3b5284b6a4b810e7fb92061788a46c3cb3383d8ecb068c13a8fab9f3cb6661bf05292a3412ca00983baa8dac98648e0989347c'
