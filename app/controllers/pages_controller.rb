5454e5e9ffb68609957a11e4eebcbeaa9335a143
Marcelo Novaes <novaes.ssa@gmail.com>
2012-01-19 16:41:04 -0200

    CSS and Pages Controller

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+	def about
+	
+	end
+	
+	def contact
+
+	end 
+	
+	def home
+    	# @news=News.all :order=> "created_at DESC",:limit => 3
+ 	end
+
+	def history
+	
+	end
+end
