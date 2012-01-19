f6898c9f04025953971d8d09b4218dfe9e4fe90d
Marcelo Novaes <novaes.ssa@gmail.com>
2012-01-19 16:09:22 -0200

    Mudando versões

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Eneej::Application.config.secret_token = '4535fd3b11e66513e17b723c3dab2f1e2d2a7fd0f0fdb39bbc0d40284428f3006c4b5d6e7f088c8abbf20fab4b2981a248a741b0d8c54dc2c6558dd90f4a1c28'
