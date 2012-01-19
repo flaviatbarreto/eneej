5df0d3105653a390af0555b8e3e94498261aab4c
Marcelo Novaes <novaes.ssa@gmail.com>
2012-01-19 16:43:57 -0200

    Removi as ações de PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,5 @@
 class PagesController < ApplicationController
-	def about
-	
-	end
-	
-	def contact
-
-	end 
-	
 	def home
     	# @news=News.all :order=> "created_at DESC",:limit => 3
  	end
-
-	def history
-	
-	end
 end
